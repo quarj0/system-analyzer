642acd9ee992aa93df9a93f9e87dfe953988816d
Owusu Ansah Kwadwo <owusuansahkwadwo24@gmail.com>
2024-10-15 12:10:34 +0000

    "Bump version: 2.0.0"

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='system-response-analyzer',
-    version='1.1.2',
+    version='2.0.0',
     author='OWUSU ANSAH K.',
     author_email='owusuansahkwadwo24@email.com',
     description='System Response Analyzer Tool',
